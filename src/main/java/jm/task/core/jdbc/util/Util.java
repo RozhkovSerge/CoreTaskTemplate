8dd0964fd7394ff39cbe8b66c10796007e9d9e0f
Serge Rozkov <rozkov_ss@mail.ru>
2021-11-09 22:53:44 +0300

    com.mysql.cj.exceptions.ConnectionIsClosedException: No operations allowed after connection closed. in line 59

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,17 +8,29 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соеденения с БД
     private static Connection connection;
+    private static Util instance;
     private final static String URL = "jdbc:mysql://localhost:3306/db_1";
     private final static String USER = "db_user";
     private final static String PASSWORD = "db_user";
 
-    public static Connection getConnection() {
+    private Util() {
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public static Util getInstance() throws SQLException {
+        if(instance == null) {
+            instance = new Util();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
         return connection;
     }
 }
